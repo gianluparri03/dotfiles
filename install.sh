15f9dc41e2ce4efb9d4197d9aa73fc1c79cd399b
gianluparri03 <gianlucaparri03@gmail.com>
2021-04-14 09:58:34 +0200

    Fixes

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 
-ln bashrc ~/.bashrc
-ln bash_aliases ~/.bash_aliases
-ln vimrc ~/.vimrc
+DIR=`dirname $(readlink -f "$0")`
+
+
+rm ~/.bashrc ~/.bash_aliases ~/.vimrc
+
+ln -s $DIR/bash/bashrc ~/.bashrc
+ln -s $DIR/bash/bash_aliases ~/.bash_aliases
+ln -s $DIR/vim/vimrc ~/.vimrc
